202e66b40459be83b05ce97e510bef1b14573e67
anilkumar75001900 <anilkumar75001900@gmail.com>
2020-12-13 16:38:24 +0530

    intial commit

---
+++ Task2.java
@@ -0,0 +1,14 @@
+    class Task2 {
+        public static void main(String[] args) {
+            int i;
+            float f,fa;
+            System.out.println("Sr. No      Temperature(Celsius)           Temperature(Fahrenheit)");
+            for(i=1;i<=101;i++){
+               f=10*(i-1);
+               fa=9*f/5+32;
+               System.out.println(+i+"             "+f+"                               "+fa);
+            }
+              }  
+       
+    
+}
